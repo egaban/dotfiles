4a8b39e69d5e592aadac4340fbf8c6a61e2acde4
Enzo Gaban <enzogaban@gmail.com>
2024-11-23 20:54:53 -0300

    neovim: add gy mapping for type definition

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,6 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+
 map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
