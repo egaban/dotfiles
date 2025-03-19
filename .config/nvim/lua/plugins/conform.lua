a49be0a57242a8c90a927efe377473884a1bd439
Enzo Gaban <git.gaban@pm.me>
2025-03-19 19:17:15 -0300

    async formatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,19 +1,14 @@
 return {
-  'stevearc/conform.nvim',
-  event = "VeryLazy",
-  opts = {
-  formatters_by_ft = {
-      lua = { "stylua" },
-    python = { "isort", "black" },
-      rust = { "rustfmt", lsp_format = "fallback" },
-    },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_format = "fallback",
-  },
-  },
-  -- config = function(_, opts) 
-  --   require("conform").setup(opts)
-  -- end
+	"stevearc/conform.nvim",
+	event = "VeryLazy",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+		},
+		format_after_save = {
+			lsp_format = "fallback",
+		},
+	},
 }
